8115b86c6f767113b87cc8c5c3f4045242aa6bf8
smile林125 <36221031+smile0125@users.noreply.github.com>
2018-04-07 18:09:35 +0800

    rui update
    增加了跳转显示图片

--- js/index.js
+++ js/index.js
@@ -1,9 +1,17 @@
 $(function () {
-    $(".bta").click(function(){
+    var arr=["image/big-img1.jpg","image/big-img2.jpg","image/big-img3.jpg",
+        "image/big-img4.jpg","image/big-img5.jpg","image/big-img6.jpg"];
+    $(".bta").click(function () {
         $(this).addClass("active").parent().siblings().children("a").removeClass("active")
     });
 
-   $(".work-img").bind("mouseenter mouseleave",function (e) {
-     $(this).find(".blue-bg").fadeToggle(200);
-   })
+    $(".work-img").bind("mouseenter mouseleave", function (e) {
+        $(this).find(".blue-bg").fadeToggle(200);
+    });
+
+    $(".showImg").each(function(index,item){
+        $(this).click(function(){
+            window.open("show.html?"+arr[index], "_block");
+        });
+    });
 });
